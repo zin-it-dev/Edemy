1182422c922457e2271150183400ccd54b60d8fa
zin-it-dev <zin.it.dev@gmail.com>
2025-10-28 23:02:07 +0700

    🐛 Fix CI Bug

--- client/web/src/pages/Outline.tsx
+++ client/web/src/pages/Outline.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import { useParams } from "react-router";
 
 import { type Outline } from "@/services/course.service";
 
 const Outline: React.FC = () => {
-  const { id } = useParams();
-
   return (
     <div>
       
